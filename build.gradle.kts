8916ba05cf014a1c67fbfa4ca998e24d1f72c72c
Peter-Crawley <peterzcrawley@gmail.com>
2022-04-02 18:50:24 +0100

    Formatting and Configuration Fixes

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,21 +1,21 @@
 import org.gradle.jvm.toolchain.JavaLanguageVersion.of
 
 plugins {
-    kotlin("jvm") version "1.6.10"
-    kotlin("kapt") version "1.6.10"
-    id("com.github.johnrengelman.shadow") version "7.1.2"
+	kotlin("jvm") version "1.6.10"
+	kotlin("kapt") version "1.6.10"
+	id("com.github.johnrengelman.shadow") version "7.1.2"
 }
 
 repositories {
-    maven("https://papermc.io/repo/repository/maven-public/") // Paper & Velocity
-    mavenCentral()
+	maven("https://papermc.io/repo/repository/maven-public/") // Paper & Velocity
+  mavenCentral()
 }
 
 dependencies {
-    compileOnly("io.papermc.paper:paper-api:1.18.2-R0.1-SNAPSHOT") // Paper
-    compileOnly("com.velocitypowered:velocity-api:3.1.1") // Velocity
+	compileOnly("io.papermc.paper:paper-api:1.18.2-R0.1-SNAPSHOT") // Paper
+	compileOnly("com.velocitypowered:velocity-api:3.1.1") // Velocity
 
-    kapt("com.velocitypowered:velocity-api:3.1.1") // Velocity
+	kapt("com.velocitypowered:velocity-api:3.1.1") // Velocity
 }
 
 java.toolchain.languageVersion.set(of(17))
